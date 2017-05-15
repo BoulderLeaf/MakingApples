7af6c748588a6b5700769203fbc21876a2caeb65
Colin Cove <ccove@pandeagames.com>
2017-05-14 20:25:04 -0700

    adding template file for config

---
+++ src/config.template.js
@@ -0,0 +1,11 @@
+export default {
+    
+    dataRoot:"url/to/root",
+    
+    github:{
+        userName:"UserName",
+        token:"6f4bc5755839s7632e3c9b86379f33f7acfde00a",
+        repo:"RepoName",
+        owner:"RepoOwner"
+    }
+}
